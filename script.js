4bebbb4e0710d81cabb6845bb0a3687ed8fd17e8
Joe <joesidious@gmail.com>
2021-05-23 09:46:27 -0700

    Modified

--- script.js
+++ script.js
@@ -2,105 +2,37 @@
 
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector(".clear-cart");
-const cartDOM = document.querySelector(".cart");
-const cartOverlay =document.querySelector(".cart-overlay");
-const cartItems = document.querySelector(".cart-items");
-const cartTotal = document.querySelector(".cart-total");
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('cart-total');
 const cartContent = document.querySelector(".cart-content");
-const productsDOM  = document.querySelector('.products-center');
+const productsDOM = document.querySelector(".products-center");
 
-//cart
-let cart =[];
+let cart = []; //cart items
 
 //getting the products
 class Products{
-  async getProducts(){
-      try {
-    let result = awaitfetch('products.json')
-    let data = await result.json();
-   let products = data.items;
-   products = products.map(item =>{
-       const {title,price} = item.fields;
-       const {id} = item.sys
-       const image = item.fieldsimage.fields.file.url;
-       return {title, price, id, image}
-   })
-   return products
-      return data;
-     }
-      catch (error)
-      {
-       console.log(error); 
-      }
+  getProducts(){
+    fetch('products.json')
   }
+
+
 }
 
-// user interface for products
-/*class UI {
-    displayProducts(products){
-        let result ='';
-        products.forEach(product => {
-            result +=  `
-            <!-- single product  -->
-            <article class="product">
-                <div class="img-container">
-             <img src=${product.image} class="product-img">
-             <button class="bag-btn" data-id=${product.id}>
-                 <i class="fas fa-shopping-cart"><img src="./icons/cart_logo.svg" width="20px">
-                 </i>add to bag
-             </button>
-         </div>
-         <h3>${product.title}</h3>
-         <h4>&#8358 ${product.price}</h4>
-            </article>
-         </div>
-    
-<!-- end of single product -->
-
-            `
-        });
-          productsDOM.innerHTML = result;
-    }
-
-} */
-
+// display (User Interface)
 class UI {
-    displayProducts(products) {
-      let result = "";
-      products.forEach(product => {
-        result += `
-     <!-- single product -->
-          <article class="product">
-            <div class="img-container">
-              <img
-                src=${product.image}
-                alt="product"
-                class="product-img"
-              />
-              <button class="bag-btn" data-id=${product.id}>
-                <i class="fas fa-shopping-cart"></i>
-                add to bag
-              </button>
-            </div>
-            <h3>${product.title}</h3>
-            <h4>$${product.price}</h4>
-          </article>
-          <!-- end of single product -->
-     `;
-      });
-      productsDOM.innerHTML = result;
-    }
+
 }
-//local storage 
+
+//local storage
 class Storage{
 
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    const ui = new UI();
-    const products = new Products();
+  const ui = new UI ();
+  const products = new Products();
 
-    //get all products
-    products.getProducts().then(data => ui.displayProducts(products));
 })
